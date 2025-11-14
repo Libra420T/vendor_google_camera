cdf7708e7f4c70b059724ab0cbd385e6e417f5e1
Libra420T <ryantf420@gmail.com>
2025-11-14 09:34:39 -0500

    Initial GoogleCamera Commit

---
+++ Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call my-dir)
+
+# GoogleCamera
+include $(CLEAR_VARS)
+LOCAL_MODULE := GoogleCamera
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := GoogleCamera/GoogleCamera.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_OPTIONAL_USES_LIBRARIES := android.ext.adservices androidx.window.extensions androidx.window.sidecar
+ifneq ($(WITH_GMS),true)
+LOCAL_OVERRIDES_PACKAGES := Aperture
+endif
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+
+# GoogleCamera Prof
+include $(CLEAR_VARS)
+LOCAL_MODULE := GoogleCamera
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := GoogleCamera/GoogleCamera.apk.prof
+Local_SUB_DIR :=GoogleCamera
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_MODULE_INSTALLABLE := false
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
